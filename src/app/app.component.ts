bc8e0a7b2cf86f022ce386e4be045bee5e9f99d2
Farhana <dfarhanabanu@gmail.com>
2019-03-19 11:16:27 +0530

    overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  overlay1 =false;
   title = 'overlay';
+selectedFruit : string;
+value:boolean;
+  fruits=["apple","mango","grapes","pineapple"];
+  onSelect(fruit){
+    this.selectedFruit=fruit;
+    this.value=true;
+  }
+  overlay(){
+    console.log("overelay clikced");
+    this.value=false;
+  }
 }
